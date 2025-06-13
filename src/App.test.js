5827752f3b0dbac45b2eff170103eedf51bb0949
Pavan Kumar <136159270+arjun7781@users.noreply.github.com>
2025-06-14 00:07:59 +0530

    Add files via upload

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Calendar header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/calendar/i);
+  expect(headerElement).toBeInTheDocument();
 });
+
+// src/App.test.js
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
